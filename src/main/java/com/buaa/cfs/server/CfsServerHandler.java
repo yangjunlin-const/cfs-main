227e67fe073118434c3d6092d1e284a20cfe74c7
root <519100949@qq.com>
2016-03-04 14:24:59 +0800

    refactor the project and add cfs proto server and client.

--- src/main/java/com/buaa/cfs/server/CfsServerHandler.java
+++ src/main/java/com/buaa/cfs/server/CfsServerHandler.java
@@ -1,7 +1,5 @@
 package com.buaa.cfs.server;
 
-import com.buaa.cfs.protobufer.CfsProto;
-import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import org.apache.commons.logging.Log;
@@ -9,28 +7,12 @@ import org.apache.commons.logging.LogFactory;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-/**
- * Created by on 12/24/15.
- */
 public class CfsServerHandler extends SimpleChannelInboundHandler {
     private static Log LOG = LogFactory.getLog(CfsServerHandler.class);
     private static AtomicLong atomicLong = new AtomicLong(0);
 
     @Override
     public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-        CfsProto.CfsMessage cfsMessage = (CfsProto.CfsMessage) msg;
-//        Message.OpMessage response = (Message.OpMessage) msg;
-//        long count = atomicLong.incrementAndGet();
-//        Message.OpMessage.Builder builder = new Message.OpMessage.Builder().setName(response.getName()).setAge(response.getAge()).setCount(++count);
-//        response = builder.build();
-//        logger.info("server add one to count , now count is : " + count);
-//        ChannelFuture channelFuture = ctx.writeAndFlush(response);
-       /* channelFuture.addListener(new ChannelFutureListener() {
-            @Override
-            public void operationComplete(ChannelFuture future) throws Exception {
-                ctx.close();
-            }
-        });*/
     }
 
     @Override
@@ -43,6 +25,5 @@ public class CfsServerHandler extends SimpleChannelInboundHandler {
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         LOG.info("---server read complete.");
         ctx.flush();
-//        ctx.close();
     }
 }
